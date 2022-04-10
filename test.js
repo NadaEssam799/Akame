302a2df0a431872bb057d2df861161a494a882a7
Nada Essam <58529416+NadaEssam799@users.noreply.github.com>
2022-04-10 05:48:06 +0200

    First Committ

---
+++ test.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $(".on").mouseenter(function(){
+  $(this).css("box-shadow", " 0px 5px 50px 4px rgba(0, 0, 0, 0.3)");
+  });
+   
+    $(".on").mouseleave(function(){
+  $(this).css("box-shadow", " 0 0px 0px rgba(0, 0, 0, 0.3)");
+  });
+});
+
+
+
+$(document).ready(function(){
+    $(".tw").mouseenter(function(){
+  $(this).css("box-shadow", " 0px 5px 50px 4px rgba(0, 0, 0, 0.3)");
+  });
+   
+    $(".tw").mouseleave(function(){
+  $(this).css("box-shadow", " 0 0px 0px rgba(0, 0, 0, 0.3)");
+  });
+});
+
+
+$(document).ready(function(){
+    $(".th").mouseenter(function(){
+  $(this).css("box-shadow", " 0px 5px 50px 4px rgba(0, 0, 0, 0.3)");
+  });
+   
+    $(".th").mouseleave(function(){
+  $(this).css("box-shadow", " 0 0px 0px rgba(0, 0, 0, 0.3)");
+  });
+});
+
+$(document).ready(function(){
+    $(".g1").mouseenter(function(){
+  $(this).css("box-shadow", " 0px 5px 50px 4px rgba(0, 0, 0, 0.3)");
+  });
+   
+    $(".g1").mouseleave(function(){
+  $(this).css("box-shadow", " 0 0px 0px rgba(0, 0, 0, 0.3)");
+  });
+});
+
+$(document).ready(function(){
+    $(".g2").mouseenter(function(){
+  $(this).css("box-shadow", " 0px 5px 50px 4px rgba(0, 0, 0, 0.3)");
+  });
+   
+    $(".g2").mouseleave(function(){
+  $(this).css("box-shadow", " 0 0px 0px rgba(0, 0, 0, 0.3)");
+  });
+});
+
+$(document).ready(function(){
+    $(".g3").mouseenter(function(){
+  $(this).css("box-shadow", " 0px 5px 50px 4px rgba(0, 0, 0, 0.3)");
+    });
+   
+    $(".g3").mouseleave(function(){
+  $(this).css("box-shadow", " 0 0px 0px rgba(0, 0, 0, 0.3)");
+  });
+});
+
+$(document).ready(function(){
+    $(".g4").mouseenter(function(){
+  $(this).css("box-shadow", " 0px 5px 50px 4px rgba(0, 0, 0, 0.3)");
+  });
+   
+    $(".g4").mouseleave(function(){
+  $(this).css("box-shadow", " 0 0px 0px rgba(0, 0, 0, 0.3)");
+  });
+});
